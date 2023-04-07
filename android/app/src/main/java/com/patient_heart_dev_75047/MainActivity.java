8605f567c25a97116f37889931e17ccb5367aebf
Crowdbotics <engineering@crowdbotics.com>
2023-04-07 00:19:43 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/patient_heart_dev_75047/MainActivity.java
@@ -0,0 +1,15 @@
+package com.patient_heart_dev_75047;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "patient_heart_dev_75047";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "patient-heart-dev-75047",
+  url: "https://patient-heart-dev-75047.botics.co",
+  api: "https://patient-heart-dev-75047.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
